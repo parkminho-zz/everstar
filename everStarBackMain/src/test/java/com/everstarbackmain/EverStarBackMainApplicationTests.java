fe0698bf2d7d065329542408d848d8d718ae83c4
jjongbbang2 <jjongbbang2@naver.com>
2024-07-22 15:02:20 +0900

    everstarBackMain init

---
+++ everStarBackMain/src/test/java/com/everstarbackmain/EverStarBackMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everstarbackmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverStarBackMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
