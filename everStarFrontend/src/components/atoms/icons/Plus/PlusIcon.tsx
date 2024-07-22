f4442ea40e1387d09920a1ff9cc17d3946a1503e
joe <j96263732@gmail.com>
2024-07-23 07:01:56 +0900

    icon 컴포넌트 다수 추가

---
+++ everStarFrontend/src/components/atoms/icons/Plus/PlusIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ReactComponent as PlusIconSVG } from '../../../../assets/icons/plus.svg';
+
+interface PlusIconProps {
+  size: 16 | 24;
+  color?: 'black' | 'gray' | 'white' | 'orange';
+}
+
+const PlusIcon: React.FC<PlusIconProps> = ({ size, color = 'black' }) => {
+  const sizeClasses = size === 16 ? 'w-4 h-4' : 'w-6 h-6';
+  const colorClasses = {
+    black: 'text-greyscaleblack-100',
+    gray: 'text-greyscaleblack-60',
+    white: 'text-greyscalewhite',
+    orange: 'text-mainprimary',
+  };
+
+  return <PlusIconSVG className={`${sizeClasses} ${colorClasses[color]}`} />;
+};
+
+export default PlusIcon;
+export type { PlusIconProps };
---
+++ everStarFrontend/src/components/atoms/icons/Settings/SettingsIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ReactComponent as SettingsIconSVG } from '../../../../assets/icons/settings.svg';
+
+interface SettingsIconProps {
+  size: 16 | 24;
+  color?: 'black' | 'gray' | 'white' | 'orange';
+}
+
+const SettingsIcon: React.FC<SettingsIconProps> = ({ size, color = 'black' }) => {
+  const sizeClasses = size === 16 ? 'w-4 h-4' : 'w-6 h-6';
+  const colorClasses = {
+    black: 'text-greyscaleblack-100',
+    gray: 'text-greyscaleblack-60',
+    white: 'text-greyscalewhite',
+    orange: 'text-mainprimary',
+  };
+
+  return <SettingsIconSVG className={`${sizeClasses} ${colorClasses[color]}`} />;
+};
+
+export default SettingsIcon;
+export type { SettingsIconProps };
