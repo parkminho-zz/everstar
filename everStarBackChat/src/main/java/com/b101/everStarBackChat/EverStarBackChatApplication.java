5090eaf4797e074453e67e907bc214adaf797473
강세현 <gedfgrdt0@gmail.com>
2024-07-22 14:54:15 +0900

    everStarBackChat init

---
+++ everStarBackChat/src/main/java/com/b101/everStarBackChat/EverStarBackChatApplication.java
@@ -0,0 +1,13 @@
+package com.b101.everStarBackChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverStarBackChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverStarBackChatApplication.class, args);
+	}
+
+}
---
+++ everStarBackChat/src/test/java/com/b101/everStarBackChat/EverStarBackChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b101.everStarBackChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverStarBackChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
