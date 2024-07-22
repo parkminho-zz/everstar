57b5769d09adf51f2b6a33a97529f5236f34a04c
jjongbbang2 <jjongbbang2@naver.com>
2024-07-22 15:07:59 +0900

    everStarBackAuth init

---
+++ everStarBackAuth/src/main/java/com/everstarbackauth/EverStarBackAuthApplication.java
@@ -0,0 +1,13 @@
+package com.everstarbackauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverStarBackAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverStarBackAuthApplication.class, args);
+	}
+
+}
---
+++ everStarBackAuth/src/test/java/com/everstarbackauth/EverStarBackAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everstarbackauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverStarBackAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
