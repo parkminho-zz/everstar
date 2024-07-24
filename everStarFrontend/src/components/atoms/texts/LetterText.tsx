963e88672118c43273267f6f4519c5ad84b5f955
고승희 <bookshelf1234@ewhain.net>
2024-07-24 12:23:15 +0900

    Feat: 태그, 텍스트 생성

---
+++ everStarFrontend/src/components/atoms/texts/LetterText.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+type LabelSize = 'large' | 'medium' | 'small';
+type Color = 'black' | 'gray' | 'white';
+
+interface ILetterText {
+  size: LabelSize;
+  color: Color;
+  children: string;
+}
+
+const fontStyle = {
+  large: 'text-[16px] font-normal leading-normal tracking-tight',
+  medium: 'text-[13px] font-normal leading-normal tracking-[-1.04px]',
+  small: 'text-[12px] font-normal leading-normal tracking-[-0.24px]',
+};
+
+const colorStyle = {
+  black: 'text-greyscaleblack-100',
+  gray: 'text-greyscaleblack-80',
+  white: 'text-greyscalewhite',
+};
+
+export const LetterText = ({ size, color, children }: ILetterText) => {
+  return (
+    <div className="max-w-full">
+      <div className={`top-0 left-0 font-Kyobo ${fontStyle[size]} ${colorStyle[color]}`}>
+        {children}
+      </div>
+    </div>
+  );
+};
