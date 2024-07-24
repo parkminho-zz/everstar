58ea31170eca3f1ee201d7ef27919b618f2c1f25
limjonghyeok <jjognbbang2@naver.com>
2024-07-25 07:11:11 +0900

    권한 수정

--- everStarBackAuth/src/main/java/com/everstarbackauth/domain/user/model/User.java
+++ everStarBackAuth/src/main/java/com/everstarbackauth/domain/user/model/User.java
@@ -2,8 +2,12 @@ package com.everstarbackauth.domain.user.model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
 
 import com.everstarbackauth.domain.user.requestDto.JoinRequestDto;
+import com.everstarbackauth.global.exception.CustomException;
+import com.everstarbackauth.global.exception.ExceptionResponse;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -21,7 +25,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id", nullable = false)
@@ -39,22 +43,26 @@ public class User {
 	@Column(name = "phone_number", nullable = false, unique = true)
 	private String phoneNumber;
 
-	@Column(name = "birth_date" , nullable = false)
+	@Column(name = "birth_date", nullable = false)
 	private LocalDate birthDate;
 
 	@Column(name = "gender", nullable = false)
 	@Enumerated(EnumType.STRING)
 	private Gender gender;
 
-	@Column(name = "quest_reception_time" , nullable = false)
+	@Column(name = "quest_reception_time", nullable = false)
 	private LocalTime questReceptionTime;
 
 	@Column(name = "role", nullable = false)
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
+	@Column(name = "is_deleted", nullable = false)
+	private boolean isDeleted;
+
 	@Builder
-	private User(String email, String password, String userName, String phoneNumber, LocalDate birthDate, Gender gender, LocalTime questReceptionTime, Role role) {
+	private User(String email, String password, String userName, String phoneNumber, LocalDate birthDate, Gender gender,
+		LocalTime questReceptionTime, Role role) {
 		this.email = email;
 		this.password = password;
 		this.userName = userName;
@@ -63,18 +71,26 @@ public class User {
 		this.gender = gender;
 		this.questReceptionTime = questReceptionTime;
 		this.role = role;
+		this.isDeleted = true;
 	}
 
-	public static User signUpUser(JoinRequestDto joinRequestDto){
+	public static User signUpUser(JoinRequestDto joinRequestDto) {
 		return User.builder()
-				.email(joinRequestDto.getEmail())
-				.password(joinRequestDto.getPassword())
-				.userName(joinRequestDto.getUserName())
-				.phoneNumber(joinRequestDto.getPhoneNumber())
-				.birthDate(joinRequestDto.getBirthDate())
-				.gender(joinRequestDto.getGender())
-				.questReceptionTime(joinRequestDto.getQuestReceptionTime())
-				.role(joinRequestDto.getRole())
-				.build();
+			.email(joinRequestDto.getEmail())
+			.password(joinRequestDto.getPassword())
+			.userName(joinRequestDto.getUserName())
+			.phoneNumber(joinRequestDto.getPhoneNumber())
+			.birthDate(joinRequestDto.getBirthDate())
+			.gender(joinRequestDto.getGender())
+			.questReceptionTime(joinRequestDto.getQuestReceptionTime())
+			.role(joinRequestDto.getRole())
+			.build();
+	}
+
+	public List<Role> getMemberRoles() {
+		if (this.role != null) {
+			return Arrays.asList(this.role);
+		}
+		throw new ExceptionResponse(CustomException.NOT_EMPTY_ROLE_EXCEPTION);
 	}
 }
--- everStarBackAuth/src/main/java/com/everstarbackauth/global/security/auth/PrincipalDetails.java
+++ everStarBackAuth/src/main/java/com/everstarbackauth/global/security/auth/PrincipalDetails.java
@@ -17,11 +17,8 @@ public class PrincipalDetails implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Collection<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new GrantedAuthority() {
-            @Override
-            public String getAuthority() {
-                return user.getRole().getRole();
-            }
+        user.getMemberRoles().forEach((r)->{
+            authorities.add(()->r.getRole());
         });
         return authorities;
     }
