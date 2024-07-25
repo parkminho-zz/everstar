4b541d269c8f528b70c9eac652a85b806198fec0
jjongbbang2 <jjongbbang2@naver.com>
2024-07-25 14:15:16 +0900

    유저 정보 추출 controller 완성

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/user/controller/UserController.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/user/controller/UserController.java
@@ -1,7 +1,17 @@
 package com.everstarbackmain.domain.user.controller;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.everstarbackmain.domain.user.responseDto.UserDetailResponseDto;
+import com.everstarbackmain.domain.user.userService.UserService;
+import com.everstarbackmain.global.util.HttpResponseUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +22,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class UserController {
 
+	private final UserService userService;
+	private final HttpResponseUtil responseUtil;
 
+	@GetMapping
+	public ResponseEntity<?> getUserDetails(Authentication authentication) {
+		UserDetailResponseDto responseDto = userService.getUserDetail(authentication);
+
+		ResponseEntity<Map<String, Object>> response = responseUtil.createResponse(responseDto);
+		log.info("main server - response : {}", response);
+		return response;
+	}
 
 }
