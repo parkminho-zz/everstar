0ce9252fab56fc6e1f2c49308ad77b117a00ec69
정현조 <j96263732@gmail.com>
2024-07-25 11:52:40 +0900

    [ feat ] Pagination 컴포넌트 구현 및 스타일링

---
+++ everStarFrontend/src/components/molecules/Pagination/Pagination.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+interface Props {
+  type: 'two' | 'four' | 'three' | 'one';
+  className?: string;
+}
+
+const Pagination: React.FC<Props> = ({ type, className }): JSX.Element => {
+  return (
+    <div className={`inline-flex items-center gap-2 px-1 py-0 relative ${className}`}>
+      <div
+        className={`relative ${type === 'one' ? 'w-2.5' : 'w-1.5'} ${type === 'one' ? 'h-2.5' : 'h-1.5'} ${
+          type === 'one' ? 'rounded-[5px]' : 'rounded-[3px]'
+        } ${type === 'one' ? 'bg-[#ff9078]' : 'bg-[#dbe4eb]'}`}
+      />
+      <div
+        className={`relative ${type === 'two' ? 'w-2.5' : 'w-1.5'} ${type === 'two' ? 'h-2.5' : 'h-1.5'} ${
+          type === 'two' ? 'rounded-[5px]' : 'rounded-[3px]'
+        } ${type === 'two' ? 'bg-[#ff9078]' : 'bg-[#dbe4eb]'}`}
+      />
+      <div
+        className={`relative ${type === 'three' ? 'w-2.5' : 'w-1.5'} ${type === 'three' ? 'h-2.5' : 'h-1.5'} ${
+          type === 'three' ? 'rounded-[5px]' : 'rounded-[3px]'
+        } ${type === 'three' ? 'bg-[#ff9078]' : 'bg-[#dbe4eb]'}`}
+      />
+      <div
+        className={`relative ${type === 'four' ? 'w-2.5' : 'w-1.5'} ${type === 'four' ? 'h-2.5' : 'h-1.5'} ${
+          type === 'four' ? 'rounded-[5px]' : 'rounded-[3px]'
+        } ${type === 'four' ? 'bg-[#ff9078]' : 'bg-[#dbe4eb]'}`}
+      />
+    </div>
+  );
+};
+
+export default Pagination;
+export type { Props };
