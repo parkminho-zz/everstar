3e8027c558c28a13e0d0badbb494977898cb8599
jjongbbang2 <jjongbbang2@naver.com>
2024-07-25 13:58:59 +0900

    userService 생성

---
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/user/userService/UserService.java
@@ -0,0 +1,20 @@
+package com.everstarbackmain.domain.user.userService;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.everstarbackmain.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class UserService {
+
+	private final UserRepository userRepository;
+
+
+}
