6b7764cfa4950462e8268723b36372e0cc7e4e73
강세현 <gedfgrdt0@gmail.com>
2024-07-26 09:32:48 +0900

    CustomException 추가 및 오타 수정

--- everStarBackMain/src/main/java/com/everstarbackmain/global/exception/CustomException.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/global/exception/CustomException.java
@@ -13,7 +13,9 @@ public enum CustomException {
 	EXPIRED_JWT_EXCEPTION(401, "ExpiredJwtException", "토큰이 만료했습니다."),
 	NOT_VALID_JWT_EXCEPTION(401, "NotValidJwtException", "토큰이 유효하지 않습니다."),
 
-	ACCESS_DENIEND_EXCEPTION(403, "AccessDeniendException", "권한이 없습니다");
+	ACCESS_DENIED_EXCEPTION(403, "AccessDeniedException", "권한이 없습니다"),
+
+	NOT_FOUND_MEMORIAL_BOOK_EXCEPTION(400, "NotFoundMemorialBookException", "메모리얼북이 존재하지 않습니다.");
 
 	private int statusNum;
 	private String errorCode;
