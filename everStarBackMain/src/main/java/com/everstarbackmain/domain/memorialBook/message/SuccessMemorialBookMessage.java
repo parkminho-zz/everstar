afa9d8dfc0aac0dfea09e6e9879e4634abccfee7
강세현 <gedfgrdt0@gmail.com>
2024-07-28 17:33:20 +0900

    Feat: 메모리얼북 심리검사결과 관련 enum 클래스 추가 및 수정

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/memorialBook/message/SuccessMemorialBookMessage.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/memorialBook/message/SuccessMemorialBookMessage.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public enum SuccessMemorialBookMessage {
 
-	SUCCESS_CHANGE_OPEN_STATUS("메모리얼북 공개 여부 수정을 성공하였습니다.");
+	SUCCESS_CHANGE_OPEN_STATUS("메모리얼북 공개 여부 수정을 성공하였습니다."),
+	SUCCESS_ADD_PSYCHOLOGICAL_TEST_RESULT("메모리얼북 심리검사결과 추가를 성공하였습니다.")
+	;
 
 	private String message;
 }
--- everStarBackMain/src/main/java/com/everstarbackmain/global/exception/CustomException.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/global/exception/CustomException.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CustomException {
 
+	WRONG_TYPE_EXCEPTION(400, "WrongTypeException", "잘못된 형식의 데이터입니다."),
+
 	NOT_EMPTY_ROLE_EXCEPTION(400, "NotEmptyRoleException", "권한이 존재하지 않습니다."),
 	NOT_FOUND_USER_EXCEPTION(400, "NotFoundUserException", "유저가 존재하지 않습니다."),
 
@@ -18,7 +20,8 @@ public enum CustomException {
 	NOT_FOUND_PET_EXCEPTION(400, "NotFoundPetException", "반려동물이 존재하지 않습니다."),
 
 	NOT_FOUND_MEMORIAL_BOOK_EXCEPTION(400, "NotFoundMemorialBookException", "메모리얼북이 존재하지 않습니다."),
-	NOT_ACTIVATED_MEMORIAL_BOOK_EXCEPTION(400, "NotActivatedMemorialBookException", "메모리얼북이 활성화 상태가 아닙니다.")
+	NOT_ACTIVATED_MEMORIAL_BOOK_EXCEPTION(400, "NotActivatedMemorialBookException", "메모리얼북이 활성화 상태가 아닙니다."),
+	NOT_MY_MEMORIAL_BOOK_EXCEPTION(400, "NotMyMemorialBookException", "사용자의 메모리얼북이 아닙니다.")
 	;
 
 	private int statusNum;
