977e010bc3003e2bb3b6b9361e9fde1d4769c32c
강세현 <gedfgrdt0@gmail.com>
2024-07-29 17:59:30 +0900

    Feat: 답변 응답 생성 dto 추가

---
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/aiAnswer/requestDto/CreateAiAnswerRequestDto.java
@@ -0,0 +1,22 @@
+package com.everstarbackmain.domain.aiAnswer.requestDto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class CreateAiAnswerRequestDto {
+
+	@NotBlank
+	private String content;
+
+	private String imageUrl;
+
+	@NotBlank
+	private String type;
+
+}
