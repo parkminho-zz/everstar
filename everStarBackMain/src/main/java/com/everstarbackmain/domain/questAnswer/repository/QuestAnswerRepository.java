9ba79657252ac1bc5c7a305cf8bd67958c84c1f0
강세현 <gedfgrdt0@gmail.com>
2024-07-29 17:56:43 +0900

    Feat: Repository 조회 메소드 추가

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/questAnswer/repository/QuestAnswerRepository.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/questAnswer/repository/QuestAnswerRepository.java
@@ -1,8 +1,12 @@
 package com.everstarbackmain.domain.questAnswer.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.everstarbackmain.domain.questAnswer.model.QuestAnswer;
 
 public interface QuestAnswerRepository extends JpaRepository<QuestAnswer, Long> {
+
+	List<QuestAnswer> findByPetId(Long petId);
 }
