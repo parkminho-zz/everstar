7ad1ac1c39131b3107d0a3f35911a6d5e742ba42
강세현 <gedfgrdt0@gmail.com>
2024-07-29 18:00:08 +0900

    Feat: 펫 상세 조회 응답 dto 추가

---
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/pet/responseDto/PetDetailResponseDto.java
@@ -0,0 +1,47 @@
+package com.everstarbackmain.domain.pet.responseDto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.everstarbackmain.domain.pet.model.Pet;
+import com.everstarbackmain.domain.user.model.Gender;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class PetDetailResponseDto {
+
+	private Long id;
+	private Integer userId;
+	private String name;
+	private Integer age;
+	private LocalDate memorialDate;
+	private String species;
+	private Gender gender;
+	private String relationship;
+	private String profileImageUrl;
+	private String introduction;
+	private Integer questIndex;
+	private LocalDateTime lastAccessTime;
+
+	public static PetDetailResponseDto createPetDetailResponseDto(Pet pet) {
+		return PetDetailResponseDto.builder()
+			.id(pet.getId())
+			.userId(pet.getUser().getId())
+			.name(pet.getName())
+			.age(pet.getAge())
+			.memorialDate(pet.getMemorialDate())
+			.species(pet.getSpecies())
+			.gender(pet.getGender())
+			.relationship(pet.getRelationship())
+			.profileImageUrl(pet.getProfileImageUrl())
+			.introduction(pet.getIntroduction())
+			.questIndex(pet.getQuestIndex())
+			.lastAccessTime(pet.getLastAccessTime())
+			.build();
+	}
+}
