3d0203c77d69cfee6c3850c6f80d0ae9d2931c07
강세현 <gedfgrdt0@gmail.com>
2024-07-29 17:50:59 +0900

    Fix: QuestAnswerId 복합키 클래스 수정

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/questAnswer/model/QuestAnswerId.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/questAnswer/model/QuestAnswerId.java
@@ -3,8 +3,16 @@ package com.everstarbackmain.domain.questAnswer.model;
 import java.io.Serializable;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class QuestAnswerId implements Serializable {
 
 	private Long petId;
