c4d94b79c8ccfe5ab66c7164c9b3b6325720e6d4
고승희 <bookshelf1234@ewhain.net>
2024-07-29 22:38:16 +0900

    Env: PWA 환경설정

--- everStarFrontend/src/index.tsx
+++ everStarFrontend/src/index.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "@fontsource/noto-sans-kr/400.css";
-import "@fontsource/noto-sans-kr/700.css";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '@fontsource/noto-sans-kr/400.css';
+import '@fontsource/noto-sans-kr/700.css';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // serviceWorkerRegistration 모듈 임포트
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
@@ -17,3 +18,9 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.unregister();  // 필요 시 주석 해제
+serviceWorkerRegistration.register();
