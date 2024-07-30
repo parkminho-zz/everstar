cf6647028a87a812287c93b1e678df6142e59ce9
강세현 <gedfgrdt0@gmail.com>
2024-07-30 10:43:48 +0900

    Fix: Integer 필드에 @NotNull 적용 수정

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/memorialBook/requestDto/MemorialBookTestResultRequestDto.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/memorialBook/requestDto/MemorialBookTestResultRequestDto.java
@@ -1,6 +1,7 @@
 package com.everstarbackmain.domain.memorialBook.requestDto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,7 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MemorialBookTestResultRequestDto {
 
-	@NotBlank
+	@NotNull
 	private Integer psychologicalTestResult;
 
 }
