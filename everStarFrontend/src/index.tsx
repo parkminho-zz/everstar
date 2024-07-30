998bf14372cf140d44caaad223b12cd444df5d42
고승희 <bookshelf1234@ewhain.net>
2024-07-30 14:35:44 +0900

    Setting: 수정3

--- everStarFrontend/src/index.tsx
+++ everStarFrontend/src/index.tsx
@@ -5,7 +5,6 @@ import '@fontsource/noto-sans-kr/700.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // serviceWorkerRegistration 모듈 임포트
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
@@ -14,9 +13,3 @@ root.render(<App />);
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.unregister();  // 필요 시 주석 해제
-// serviceWorkerRegistration.register();
