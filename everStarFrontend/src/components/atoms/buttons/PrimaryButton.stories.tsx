37490856e7b53f71397e4a80ea132cf372382d2c
정현조 <j96263732@gmail.com>
2024-07-30 13:54:06 +0900

    Refactor: primary 버튼 export 및 디자인  수정

--- everStarFrontend/src/components/atoms/buttons/PrimaryButton.stories.tsx
+++ everStarFrontend/src/components/atoms/buttons/PrimaryButton.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
-import PrimaryButton from './PrimaryButton';
+import { PrimaryButton } from './PrimaryButton';
 const meta = {
   title: 'Atoms/Buttons/PrimaryButton',
   component: PrimaryButton,
