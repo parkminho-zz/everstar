a341ebc47462d24e18b537d4607e13d1a58994cf
고승희 <bookshelf1234@ewhain.net>
2024-07-30 14:22:19 +0900

    수정

--- everStarFrontend/src/App.tsx
+++ everStarFrontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
-import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 
 import { Header } from './components/molecules/Header/Header';
@@ -12,7 +11,6 @@ function App() {
         <Header type="default" />
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
         </Routes>
       </div>
