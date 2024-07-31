02630eecfd2a85d0768d1d0f44d10ff8dc65ed1e
강세현 <gedfgrdt0@gmail.com>
2024-07-31 16:13:42 +0900

    Feat: 서비스에 OpenAI API에 종합 감정 분석 요청하는 기능 관련 테스트 추가

--- everStarBackMain/src/test/java/com/everstarbackmain/domain/questAnswer/service/QuestAnswerServiceTest.java
+++ everStarBackMain/src/test/java/com/everstarbackmain/domain/questAnswer/service/QuestAnswerServiceTest.java
@@ -20,6 +20,7 @@ import org.springframework.security.core.Authentication;
 
 import com.everstarbackmain.domain.memorialBook.model.MemorialBook;
 import com.everstarbackmain.domain.memorialBook.util.MemorialBookScheduler;
+import com.everstarbackmain.domain.openai.util.OpenAiClient;
 import com.everstarbackmain.domain.pet.model.Pet;
 import com.everstarbackmain.domain.pet.repository.PetRepository;
 import com.everstarbackmain.domain.pet.requestDto.CreatePetRequestDto;
@@ -58,7 +59,11 @@ class QuestAnswerServiceTest {
 	private NaverCloudClient naverCloudClient;
 
 	@Mock
+	private OpenAiClient openAiClient;
+
+	@Mock
 	private TaskScheduler taskScheduler;
+
 
 	@Mock
 	private Authentication authentication;
@@ -173,7 +178,7 @@ class QuestAnswerServiceTest {
 		given(naverCloudClient.analyseSentiment(anyString()))
 			.willReturn(SentimentAnalysisResult.createSentimentAnalysisResult(0.1, 0.2, 0.7));
 		given(sentimentAnalysisRepository.findByPetId(anyLong()))
-			.willReturn(Optional.empty()); // SentimentAnalysis가 존재하지 않는 경우
+			.willReturn(Optional.empty());
 
 		// when
 		ExceptionResponse exceptionResponse = assertThrows(ExceptionResponse.class, () -> {
@@ -183,4 +188,49 @@ class QuestAnswerServiceTest {
 		// then
 		assertEquals(CustomException.NOT_FOUND_SENTIMENT_ANALYSIS_EXCEPTION, exceptionResponse.getCustomException());
 	}
+
+	@Test
+	@DisplayName("퀘스트_49일차_답변_생성_후_OPENAI_API_메서드_호출_테스트")
+	public void 퀘스트_49일차_답변_생성_후_OPENAI_API_메서드_호출_테스트() {
+		// given
+		for (int i = 0; i < 48; i++) {
+			pet.plusQuestIndex();
+		}
+		given(authentication.getPrincipal()).willReturn(principalDetails);
+		given(principalDetails.getUser()).willReturn(user);
+		given(petRepository.findById(anyLong())).willReturn(Optional.of(pet));
+		given(sentimentAnalysisRepository.findByPetId(anyLong())).willReturn(Optional.of(sentimentAnalysis));
+		given(naverCloudClient.analyseSentiment(anyString())).willReturn(Optional.of(sentimentAnalysisResult).get());
+
+		// when
+		questAnswerService.createQuestAnswer(authentication, 1L);
+
+		// then
+		verify(openAiClient).analysisTotalSentiment(sentimentAnalysis);
+	}
+
+	@Test
+	@DisplayName("퀘스트_49일차_답변_생성_후_OPENAI_API_EXCEPTION_발생_테스트")
+	public void 퀘스트_49일차_답변_생성_후_OPENAI_API_EXCEPTION_발생_테스트() {
+		// given
+		for (int i = 0; i < 48; i++) {
+			pet.plusQuestIndex();
+		}
+		given(authentication.getPrincipal()).willReturn(principalDetails);
+		given(principalDetails.getUser()).willReturn(user);
+		given(petRepository.findById(anyLong())).willReturn(Optional.of(pet));
+		given(sentimentAnalysisRepository.findByPetId(anyLong())).willReturn(Optional.of(sentimentAnalysis));
+		given(naverCloudClient.analyseSentiment(anyString())).willReturn(Optional.of(sentimentAnalysisResult).get());
+		given(openAiClient.analysisTotalSentiment(any(SentimentAnalysis.class)))
+			.willThrow(new ExceptionResponse(CustomException.OPENAI_API_EXCEPTION));
+
+		// when
+		ExceptionResponse exceptionResponse = assertThrows(ExceptionResponse.class, () -> {
+			questAnswerService.createQuestAnswer(authentication, 1L);
+		});
+
+		// then
+		assertEquals(CustomException.OPENAI_API_EXCEPTION, exceptionResponse.getCustomException());
+	}
+
 }
