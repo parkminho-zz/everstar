54832b1c5cebdb953724123821fc313f0b095d6d
kimdoi <thisisdoemail@gmail.com>
2024-08-02 00:38:18 +0900

    Feat: pet 모델 성별 ENUM클래스 추가

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/pet/model/Gender.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/pet/model/Gender.java
@@ -1,4 +1,15 @@
 package com.everstarbackmain.domain.pet.model;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public enum Gender {
+
+	MALE("MALE"),
+	FEMALE("FEMALE");
+
+	private final String gender;
 }
