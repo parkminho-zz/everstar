50bcc364b87f61a61c1c244c45a2872f4ec83b83
강세현 <gedfgrdt0@gmail.com>
2024-08-02 08:55:14 +0900

    Feat: 일기 작성 파일 추가 기능 컨트롤러 추가

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/diary/controller/DiaryController.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/diary/controller/DiaryController.java
@@ -6,9 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.everstarbackmain.domain.diary.message.SuccessDiaryMessage;
 import com.everstarbackmain.domain.diary.requestDto.CreateDiaryRequestDto;
@@ -30,8 +31,9 @@ public class DiaryController {
 
 	@PostMapping
 	public ResponseEntity<Map<String, Object>> createDiary(@PathVariable("memorialbook-id") Long memorialBookId,
-		Authentication authentication, @RequestBody @Valid CreateDiaryRequestDto requestDto) {
-		diaryService.createDiary(authentication, memorialBookId, requestDto);
+		Authentication authentication, @RequestPart @Valid CreateDiaryRequestDto requestDto,
+		@RequestPart(required = false) MultipartFile imageFile) {
+		diaryService.createDiary(authentication, memorialBookId, requestDto, imageFile);
 		ResponseEntity<Map<String, Object>> response = responseUtil.createResponse(
 			SuccessDiaryMessage.SUCCESS_CREATE_DIARY);
 		log.info("main server - request : {}", requestDto);
