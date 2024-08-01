3c324c508f653dfd9607a336b4789e5828223c86
limjonghyeok <jjognbbang2@naver.com>
2024-08-01 23:17:49 +0900

    Feature : petLetterService 작성

---
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/petterLetter/service/PetLetterService.java
@@ -0,0 +1,18 @@
+package com.everstarbackmain.domain.petterLetter.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.everstarbackmain.domain.petterLetter.repository.PetLetterRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Slf4j(topic = "elk")
+public class PetLetterService {
+
+	private final PetLetterRepository petLetterRepository;
+}
