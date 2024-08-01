e8c5e6b688cc1df22d00c2e74cbcdc547f900268
kimdoi <thisisdoemail@gmail.com>
2024-08-01 22:29:29 +0900

    Feat:등록 반려동물 ResponseDto

---
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/pet/responseDto/EnrolledPetsResponseDto.java
@@ -0,0 +1,24 @@
+package com.everstarbackmain.domain.pet.responseDto;
+
+import com.everstarbackmain.domain.pet.model.Pet;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EnrolledPetsResponseDto {
+	private String profileImageUrl;
+	private String name;
+
+	public static EnrolledPetsResponseDto createEnrolledResponseDto(Pet pet) {
+		return EnrolledPetsResponseDto.builder()
+			.profileImageUrl(pet.getProfileImageUrl())
+			.name(pet.getName())
+			.build();
+	}
+}
