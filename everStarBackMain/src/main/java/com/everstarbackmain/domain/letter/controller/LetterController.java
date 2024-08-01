a841cf7d59f2f5f3866f173857e0f8e2cdbc5f40
jjongbbang2 <jjongbbang2@naver.com>
2024-08-01 14:09:09 +0900

    FEATURE: writeLetterRequestDto 작성

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/letter/controller/LetterController.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/letter/controller/LetterController.java
@@ -3,14 +3,17 @@ package com.everstarbackmain.domain.letter.controller;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.everstarbackmain.domain.letter.requestDto.WriteLetterRequestDto;
 import com.everstarbackmain.domain.letter.service.LetterService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,6 +26,8 @@ public class LetterController {
 	private final LetterService letterService;
 
 	@PostMapping
-	public ResponseEntity<Map<String, Object>> writeLetter(@PathVariable int petId, @RequestBody) {
+	public ResponseEntity<Map<String, Object>> writeLetter(Authentication authentication, @PathVariable("pet-id") long petId, @RequestBody @Valid WriteLetterRequestDto requestDto) {
+
+
 	}
 }
