3b3a0888c342e6e085b4b3d5f4cdea11044159a4
kimdoi <thisisdoemail@gmail.com>
2024-08-01 22:46:16 +0900

    Feat: 반려동물 커스텀 에러 추가

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/pet/message/SuccessPetMessage.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/pet/message/SuccessPetMessage.java
@@ -9,7 +9,8 @@ import lombok.Getter;
 public enum SuccessPetMessage {
 
 	SUCCESS_CREATE_PET("반려동물 추가를 성공하였습니다."),
-	SUUCESS_UPDATE_PET_INTRODUCTION("반려동물 소개글이 수정 되었습니다.");
+	SUCESS_UPDATE_PET_INTRODUCTION("반려동물 소개글이 수정 되었습니다."),
+	SUCESS_GET_ALL_ENROLLED_PET("등록된 반려동물 목록 조회성공");
 
 	private String message;
 }
