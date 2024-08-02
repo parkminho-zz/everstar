c7f3fdddf742f2bb3174fd835ddfd64fc5936aa9
jjongbbang2 <jjongbbang2@naver.com>
2024-08-02 14:43:31 +0900

    Feature : petLetter responseDto 작성

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/petterLetter/responseDto/PetLetterResponseDto.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/petterLetter/responseDto/PetLetterResponseDto.java
@@ -1,18 +1,25 @@
 package com.everstarbackmain.domain.petterLetter.responseDto;
 
 import com.everstarbackmain.domain.petterLetter.model.PetLetter;
+import com.querydsl.core.annotations.QueryProjection;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 @Getter
 public class PetLetterResponseDto {
 	private long petLetterId;
 	private boolean isRead;
+
+
+	@QueryProjection
+	public PetLetterResponseDto(Long id, Boolean isRead) {
+		this.petLetterId = id;
+		this.isRead = isRead;
+	}
 
 	public static PetLetterResponseDto fromPetLetter(PetLetter petLetter) {
 		return PetLetterResponseDto.builder()
