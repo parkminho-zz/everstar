ca06850a16cf3ed6d324ec9cd0e82bb0d3079b6c
joe <j96263732@gmail.com>
2024-08-04 04:04:36 +0900

    feat : OAuthCallback 링크를 통해, 리다이렉트 밑 토큰 수집

--- everStarFrontend/src/pages/OAuthCallback.tsx
+++ everStarFrontend/src/pages/OAuthCallback.tsx
@@ -4,13 +4,16 @@ import { useDispatch } from 'react-redux';
 import { SetUser, SetToken } from 'store/Auth';
 
 const fetchUserInfo = async (token: string) => {
-  const response = await fetch('https://i11b101.p.ssafy.io/api/accounts/users', {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
+  const response = await fetch(
+    'https://i11b101.p.ssafy.io/api/accounts/users',
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
     },
-  });
+  );
 
   if (!response.ok) {
     throw new Error('Failed to fetch user info');
@@ -25,24 +28,27 @@ export const OAuthCallback: React.FC = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(location.search);
-    const token = urlParams.get('token'); // 토큰을 URL에서 추출
+    const pathSegments = location.pathname.split('/');
+    const token = pathSegments[pathSegments.length - 1]; // 마지막 경로 세그먼트에서 토큰 추출
 
     if (token) {
+      console.log('Extracted token:', token); // 토큰 로그 추가
       // 토큰을 Redux에 저장
       dispatch(SetToken(token));
 
       // 사용자 정보 조회
       fetchUserInfo(token)
         .then((user) => {
+          console.log('Fetched user info:', user); // 사용자 정보 로그 추가
           dispatch(
             SetUser({
               ...user,
-              //   birthDate: new Date(user.birthDate), // birthDate를 Date 객체로 변환
-              //   questReceptionTime: new Date(user.questReceptionTime), // questReceptionTime을 Date 객체로 변환
+              // 필요한 경우 birthDate와 questReceptionTime 변환
+              // birthDate: new Date(user.birthDate),
+              // questReceptionTime: new Date(user.questReceptionTime),
             }),
           );
-          navigate('/earth');
+          navigate('/profile');
         })
         .catch((error) => {
           console.error('Error fetching user info:', error);
