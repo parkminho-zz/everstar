f2daa9197a2f74b9094dc85a0ec53e93598e5d1b
강세현 <gedfgrdt0@gmail.com>
2024-08-03 19:08:11 +0900

    Feat: JwtValueConfig 추가

---
+++ everStarBackChat/src/main/java/com/b101/everStarBackChat/global/config/JwtValueConfig.java
@@ -0,0 +1,30 @@
+package com.b101.everStarBackChat.global.config;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import io.jsonwebtoken.Jwts;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+
+@Configuration
+@Getter
+public class JwtValueConfig {
+	private SecretKey key;
+
+	@Value("${jwt.secret}")
+	private String secretKey;
+
+	@Value("${jwt.access-token-expire}")
+	private String accessTokenExpire;
+
+	@PostConstruct
+	private void init(){
+		this.key =  new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+	}
+}
