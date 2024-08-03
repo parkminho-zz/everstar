28aec7b84106ad2ff8868754839aacdb4d2c3372
kim doi <thisisdoemail@gmail.com>
2024-08-03 20:41:18 +0900

    Feat: 마이페이지 펫 정보 responseDto생성

---
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/pet/responseDto/MyPagePetInfoResponseDto.java
@@ -0,0 +1,13 @@
+package com.everstarbackmain.domain.pet.responseDto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class MyPagePetInfoResponseDto {
+
+}
