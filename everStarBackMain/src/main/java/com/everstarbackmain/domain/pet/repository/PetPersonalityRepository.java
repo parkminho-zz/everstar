9ab50ae49c490c761424df73c6ee78458c32ba28
kim doi <thisisdoemail@gmail.com>
2024-08-03 22:23:20 +0900

    Feat: 마이페이지 펫 정보 responseDto 작성

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/pet/repository/PetPersonalityRepository.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/pet/repository/PetPersonalityRepository.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.everstarbackmain.domain.pet.model.PetPersonality;
-import com.everstarbackmain.domain.questAnswer.model.QuestAnswer;
 
 public interface PetPersonalityRepository extends JpaRepository<PetPersonality, Long> {
-	List<PetPersonality> findPetPersonalitiesByAnd(Long petId, Boolean isDeleted);
+
+	List<PetPersonality> findAllByPetIdAndIsDeleted(Long petId, Boolean isDeleted);
+
 
 }
