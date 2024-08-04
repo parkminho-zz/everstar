5853ebcc3c72776b44b1f76b88ac20db5275bd86
limjonghyeok <jjognbbang2@naver.com>
2024-08-05 01:21:25 +0900

    Feature : petLetterController 작성

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/cheeringMessage/controller/CheeringMessageController.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/cheeringMessage/controller/CheeringMessageController.java
@@ -1,10 +1,13 @@
 package com.everstarbackmain.domain.cheeringMessage.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.everstarbackmain.domain.cheeringMessage.requestDto.CreateCheeringMessageRequestDto;
 import com.everstarbackmain.domain.cheeringMessage.service.CheeringMessageService;
 
 import lombok.RequiredArgsConstructor;
@@ -19,6 +22,8 @@ public class CheeringMessageController {
 	private final CheeringMessageService cheeringMessageService;
 
 	@PostMapping
-	public void createCheeringMessage(@PathVariable("pet-id") Long petId, )
+	public void createCheeringMessage(Authentication authentication, @PathVariable("pet-id") Long petId, @RequestBody CreateCheeringMessageRequestDto requestDto) {
+
+	}
 
 }
