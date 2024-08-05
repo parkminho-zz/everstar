4abf9382665ffd05bac8516aaef00e554d92f732
jjongbbang2 <jjongbbang2@naver.com>
2024-08-05 16:28:59 +0900

    주소 변경

--- everStarBackChat/src/main/java/com/b101/everStarBackChat/domain/webrtc/controller/WebRtcController.java
+++ everStarBackChat/src/main/java/com/b101/everStarBackChat/domain/webrtc/controller/WebRtcController.java
@@ -17,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/sessions")
+@RequestMapping("/api/chat/sessions")
 @Slf4j(topic = "elk")
 public class WebRtcController {
 
