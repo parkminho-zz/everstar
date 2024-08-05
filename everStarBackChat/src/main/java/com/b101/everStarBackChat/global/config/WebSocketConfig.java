ace39ca3ef8479813b06077a2a3f78adaa27c279
강세현 <gedfgrdt0@gmail.com>
2024-08-06 08:47:33 +0900

    Fix: stomp prefix 주소 변경

--- everStarBackChat/src/main/java/com/b101/everStarBackChat/global/config/WebSocketConfig.java
+++ everStarBackChat/src/main/java/com/b101/everStarBackChat/global/config/WebSocketConfig.java
@@ -12,8 +12,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/sub");
-		config.setApplicationDestinationPrefixes("/pub");
+		config.enableSimpleBroker("/api/chat/sub");
+		config.setApplicationDestinationPrefixes("/api/chat/pub");
 	}
 
 	@Override
