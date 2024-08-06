0a63dca64cc09a9b631617fb3cc186218670d74e
강세현 <gedfgrdt0@gmail.com>
2024-08-06 22:47:54 +0900

    Test: QuestAnswerService TextToText ai 답변 생성 기능 관련 테스트

--- everStarBackMain/src/test/java/com/everstarbackmain/domain/questAnswer/service/QuestAnswerServiceTest.java
+++ everStarBackMain/src/test/java/com/everstarbackmain/domain/questAnswer/service/QuestAnswerServiceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.BDDMockito.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,14 +23,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.everstarbackmain.domain.aiAnswer.repository.AiAnswerRepository;
 import com.everstarbackmain.domain.memorialBook.model.MemorialBook;
 import com.everstarbackmain.domain.memorialBook.util.MemorialBookScheduler;
+import com.everstarbackmain.domain.pet.repository.PetPersonalityRepository;
 import com.everstarbackmain.domain.pet.repository.PetRepository;
 import com.everstarbackmain.domain.quest.model.Quest;
 import com.everstarbackmain.domain.quest.model.QuestType;
 import com.everstarbackmain.domain.quest.repository.QuestRepository;
 import com.everstarbackmain.domain.questAnswer.model.QuestAnswer;
 import com.everstarbackmain.domain.questAnswer.model.QuestAnswerType;
+import com.everstarbackmain.domain.questAnswer.model.QuestAnswerTypeNo;
 import com.everstarbackmain.domain.questAnswer.requestDto.CreateAnswerRequestDto;
 import com.everstarbackmain.global.openai.util.OpenAiClient;
 import com.everstarbackmain.domain.pet.model.Pet;
@@ -62,10 +66,16 @@ class QuestAnswerServiceTest {
 	private PetRepository petRepository;
 
 	@Mock
+	private PetPersonalityRepository petPersonalityRepository;
+
+	@Mock
 	private MemorialBookScheduler memorialBookScheduler;
 
 	@Mock
 	private QuestAnswerRepository questAnswerRepository;
+
+	@Mock
+	private AiAnswerRepository aiAnswerRepository;
 
 	@Mock
 	private SentimentAnalysisRepository sentimentAnalysisRepository;
@@ -90,11 +100,14 @@ class QuestAnswerServiceTest {
 
 	private User user;
 	private Pet pet;
+	private List<String> petPersonalities;
 	private MemorialBook memorialBook;
 	private SentimentAnalysis sentimentAnalysis;
 	private SentimentAnalysisResult sentimentAnalysisResult;
 	private Quest quest;
 	private CreateAnswerRequestDto createAnswerRequestDto;
+	private CreateAnswerRequestDto createTextAnswerRequestDto;
+	private QuestAnswer textQuestAnswer;
 
 	@BeforeEach
 	public void setup() {
@@ -103,50 +116,16 @@ class QuestAnswerServiceTest {
 		pet = Pet.createPet(user, new CreatePetRequestDto("petName", 10,
 			LocalDate.of(1990, 1, 1), "species", PetGender.MALE,
 			"relationship", List.of("개구쟁이", "귀염둥이")), "profileImageUrl");
+		petPersonalities = List.of("개구쟁이, 귀염둥이");
 		memorialBook = MemorialBook.createMemorialBook(pet);
 		sentimentAnalysis = SentimentAnalysis.createSentimentAnalysis(pet);
 		sentimentAnalysisResult = SentimentAnalysisResult.createSentimentAnalysisResult(0.1, 0.3, 0.6);
 		quest = new Quest("content", QuestType.TEXT);
 		createAnswerRequestDto = new CreateAnswerRequestDto("content", QuestAnswerType.TEXT_IMAGE.getType());
+		createTextAnswerRequestDto = new CreateAnswerRequestDto("content", QuestAnswerType.TEXT.getType());
+		textQuestAnswer = QuestAnswer.createTextQuestAnswer(pet, quest, createTextAnswerRequestDto);
 
 		ReflectionTestUtils.setField(pet, "id", 1L);
-	}
-
-	@Test
-	@DisplayName("퀘스트_답변_생성_성공_테스트")
-	public void 퀘스트_답변_생성_성공_테스트() {
-		// given
-		MultipartFile imageFile = Mockito.mock(MultipartFile.class);
-		given(authentication.getPrincipal()).willReturn(principalDetails);
-		given(principalDetails.getUser()).willReturn(user);
-		given(petRepository.findByIdAndIsDeleted(anyLong(), anyBoolean())).willReturn(Optional.of(pet));
-		given(questRepository.findById(anyLong())).willReturn(Optional.of(quest));
-
-		// when
-		questAnswerService.createQuestAnswer(authentication, 1L, 1L, createAnswerRequestDto, imageFile);
-
-		// then
-		BDDMockito.then(questAnswerRepository).should(times(1)).save(any(QuestAnswer.class));
-		BDDMockito.then(s3UploadUtil).should(times(1)).saveFile(imageFile);
-	}
-
-	@Test
-	@DisplayName("퀘스트_답변_생성_퀘스트_없음_예외_테스트")
-	public void 퀘스트_답변_생성_퀘스트_없음_예외_테스트() {
-		// given
-		MultipartFile imageFile = Mockito.mock(MultipartFile.class);
-		given(authentication.getPrincipal()).willReturn(principalDetails);
-		given(principalDetails.getUser()).willReturn(user);
-		given(petRepository.findByIdAndIsDeleted(anyLong(), anyBoolean())).willReturn(Optional.of(pet));
-		given(questRepository.findById(anyLong())).willReturn(Optional.empty());
-
-		// when
-		ExceptionResponse exceptionResponse = assertThrows(ExceptionResponse.class, () -> {
-			questAnswerService.createQuestAnswer(authentication, 1L, 1L, createAnswerRequestDto, imageFile);
-		});
-
-		// then
-		assertEquals(CustomException.NOT_FOUND_QUEST_EXCEPTION, exceptionResponse.getCustomException());
 	}
 
 	@Test
@@ -306,6 +285,30 @@ class QuestAnswerServiceTest {
 
 		// then
 		assertEquals(CustomException.OPENAI_API_EXCEPTION, exceptionResponse.getCustomException());
+	}
+
+	@Test
+	@DisplayName("퀘스트_답변_생성_후_TEXT_TO_TEXT_타입일_경우_관련_OPENAI_API_호출_테스트")
+	public void 퀘스트_답변_생성_후_TEXT_TO_TEXT_타입일_경우_관련_OPENAI_API_호출_테스트() {
+		given(authentication.getPrincipal()).willReturn(principalDetails);
+		given(principalDetails.getUser()).willReturn(user);
+		given(petRepository.findByIdAndIsDeleted(anyLong(), anyBoolean())).willReturn(Optional.of(pet));
+		given(questRepository.findById(anyLong())).willReturn(Optional.of(quest));
+		given(petPersonalityRepository.findPersonalityValuesByPetIdAndIsDeleted(anyLong(), anyBoolean())).willReturn(
+			petPersonalities);
+		ReflectionTestUtils.setField(quest, "id", 2L);
+
+		mockStatic(QuestAnswerTypeNo.class);
+		given(QuestAnswerTypeNo.findTypeByQuestNumber(anyLong())).willReturn(
+			Optional.of(QuestAnswerTypeNo.TEXT_TO_TEXT.getType()));
+		mockStatic(QuestAnswer.class);
+		given(QuestAnswer.createTextQuestAnswer(any(), any(), any())).willReturn(textQuestAnswer);
+
+		// when
+		questAnswerService.createQuestAnswer(authentication, 1L, 2L, createTextAnswerRequestDto, null);
+
+		// then
+		verify(openAiClient).writePetTextToTextAnswer(user, pet, petPersonalities, quest, textQuestAnswer);
 	}
 
 }
