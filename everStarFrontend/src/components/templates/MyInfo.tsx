bb2b83c3a666d3ba1f0241be842a103804e0ac1f
joe <j96263732@gmail.com>
2024-08-07 00:16:47 +0900

    feat: MyInfo 컴포넌트 Redux 통합 및 데이터 패칭 로직 추가
    - Redux 스토어와 통합하여 사용자 정보 및 애완동물 세부사항을 동적으로 패칭하도록 수정
    -  훅 추가하여 페이지 네비게이션 구현
    - Redux 스토어에서 데이터를 받아와서 컴포넌트를 동적으로 업데이트하도록 변경
    - Storybook 스토리에 Redux Provider 및 PersistGate를 추가하여 상태 관리 구현
    -  컴포넌트를 Redux 스토어에서 가져온 데이터로 업데이트하고 애완동물 선택 처리 기능 추가
    - 개발을 위한 디버그 로그 추가

--- everStarFrontend/src/components/templates/MyInfo.tsx
+++ everStarFrontend/src/components/templates/MyInfo.tsx
@@ -1,89 +1,155 @@
-import React from 'react';
-import { useMediaQuery } from 'react-responsive';
-import { Header } from 'components/molecules/Header/Header';
-import { Footer } from 'components/molecules/Footer/Footer';
-import { Glass } from 'components/molecules/Glass/Glass';
-import bgImage from 'assets/images/bg-login.webp';
-import { UserProfile, UserProfileProps } from 'components/organics/Profile/UserProfile';
-
-// JSON 데이터 (예시)
-const petInfo = {
-  뚜뚜: {
-    name: '뚜뚜',
-    birthdate: '2017-01-01',
-    gender: '여성',
-    breed: '푸들',
-    color: '갈색',
-    personality: ['사랑스러움', '활발함', '애교쟁이'],
-  },
-  타이틀1: {
-    name: '타이틀1',
-    birthdate: '2018-02-02',
-    gender: '남성',
-    breed: '시바견',
-    color: '검정색',
-    personality: ['용맹함', '충성심', '경계심'],
-  },
-  타이틀2: {
-    name: '타이틀2',
-    birthdate: '2019-03-03',
-    gender: '여성',
-    breed: '골든 리트리버',
-    color: '노란색',
-    personality: ['친절함', '지혜로움', '활동적'],
-    avatarSrc: 'https://via.placeholder.com/120', // 추가된 부분
-  },
-  타이틀3: {
-    name: '타이틀3',
-    birthdate: '2020-04-04',
-    gender: '남성',
-    breed: '코카 스패니얼',
-    color: '흰색',
-    personality: ['상냥함', '조용함', '온순함'],
-  },
-};
-
-const userProfileData: UserProfileProps = {
-  headerText: '마이 페이지',
-  largeButtonText: 'Save',
-  smallButtonText: '핸드폰 번호 수정하기',
-  showPrimaryButton: true,
-  userInfo: {
-    name: '김철수',
-    birthdate: '1990-01-01',
-    gender: '남성',
-    email: 'example@example.com',
-    phone: '010-1234-5678',
-  },
-  petOptions: Object.keys(petInfo),
-  petInfo: petInfo,
-};
+// src/components/templates/MyInfo.tsx
+import React, { useEffect, useMemo, useCallback, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState, AppDispatch } from 'store/Store';
+import { useFetchPets, useFetchPetDetails } from 'hooks/usePets';
+import { useFetchUserInfo } from 'hooks/useAuth';
+import { useNavigate } from 'react-router-dom'; // useNavigate 추가
+import {
+  setPets,
+  setPetDetails,
+  setSelectedPetId,
+} from 'store/slices/petSlice';
+import { setUser } from 'store/slices/authSlice';
+import {
+  UserProfile,
+  UserProfileProps,
+} from 'components/organics/Profile/UserProfile';
 
 export const MyInfo: React.FC = () => {
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 768px)' });
-  const isMobile = useMediaQuery({ query: '(max-width: 480px)' });
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate(); // useNavigate 훅 사용
+  const token = useSelector((state: RootState) => state.auth.accessToken);
+  const pets = useSelector((state: RootState) => state.pet.pets);
+  const userInfo = useSelector((state: RootState) => state.auth.userInfo);
+  const petDetails = useSelector((state: RootState) => state.pet.petDetails);
+  const selectedPetId = useSelector(
+    (state: RootState) => state.pet.selectedPetId,
+  );
 
-  const headerType = isMobile ? 'mobile-mypage' : isTabletOrMobile ? 'tablet-mypage' : 'mypage';
+  const [initialLoad, setInitialLoad] = useState(true);
 
-  const footerType = isMobile ? 'mobile' : isTabletOrMobile ? 'tablet' : 'desktop';
+  const {
+    data: petsData,
+    isLoading: isPetsLoading,
+    error: petsError,
+  } = useFetchPets(token);
+  const {
+    data: userData,
+    isLoading: isUserLoading,
+    error: userError,
+  } = useFetchUserInfo(token);
+  const {
+    data: petDetailsData,
+    isLoading: isPetDetailsLoading,
+    error: petDetailsError,
+  } = useFetchPetDetails(selectedPetId !== -1 ? selectedPetId : null, token);
+
+  useEffect(() => {
+    if (petsData) {
+      dispatch(setPets(petsData));
+    }
+  }, [petsData, dispatch]);
+
+  useEffect(() => {
+    if (userData) {
+      dispatch(setUser(userData));
+    }
+  }, [userData, dispatch]);
+
+  useEffect(() => {
+    if (initialLoad && pets.length > 0) {
+      dispatch(setSelectedPetId(pets[0].id));
+      setInitialLoad(false);
+    }
+  }, [initialLoad, pets, dispatch]);
+
+  useEffect(() => {
+    if (selectedPetId !== -1 && petDetailsData) {
+      dispatch(setPetDetails(petDetailsData));
+    }
+  }, [selectedPetId, petDetailsData, dispatch]);
+
+  // 디버그 로그 추가
+  console.log('Token:', token);
+  console.log('Pets:', pets);
+  console.log('User Info:', userInfo);
+  console.log('Pet Details:', petDetails);
+  console.log('Selected Pet ID:', selectedPetId);
+
+  const getGenderText = useCallback((gender: string) => {
+    switch (gender) {
+      case 'MALE':
+        return '남성';
+      case 'FEMALE':
+        return '여성';
+      default:
+        return 'unknown';
+    }
+  }, []);
+
+  const petOptions = useMemo(() => pets.map((pet) => pet.name), [pets]);
+
+  const petInfo = useMemo(() => {
+    return petDetails
+      ? {
+          [petDetails.name]: {
+            id: petDetails.id,
+            userId: petDetails.userId,
+            name: petDetails.name,
+            age: petDetails.age,
+            memorialDate: petDetails.memorialDate || 'unknown',
+            gender: getGenderText(petDetails.gender) || 'unknown',
+            species: petDetails.species || 'unknown',
+            relationship: petDetails.relationship,
+            personalities: petDetails.personalities || [],
+            profileImageUrl: petDetails.profileImageUrl,
+          },
+        }
+      : {};
+  }, [petDetails, getGenderText]);
+
+  const onPetSelect = useCallback(
+    (name: string) => {
+      const selectedPet = pets.find((pet) => pet.name === name);
+      if (selectedPet && selectedPet.id !== selectedPetId) {
+        dispatch(setSelectedPetId(selectedPet.id));
+      }
+    },
+    [dispatch, pets, selectedPetId],
+  );
+
+  if (isPetsLoading || isUserLoading || isPetDetailsLoading)
+    return <div>로딩 중...</div>;
+  if (petsError) return <div className='text-red-500'>{petsError.message}</div>;
+  if (userError) return <div className='text-red-500'>{userError.message}</div>;
+  if (petDetailsError)
+    return <div className='text-red-500'>{petDetailsError.message}</div>;
+  if (!userInfo) return <div>사용자 정보를 불러오는 중...</div>;
+
+  const userProfileData: UserProfileProps = {
+    headerText: '마이 페이지',
+    largeButtonText: '저장',
+    smallButtonText: '핸드폰 번호 수정하기',
+    showPrimaryButton: true,
+    userInfo: {
+      name: userInfo.userName || '', // userInfo에서 직접 가져오기
+      birthdate: userInfo.birthDate || '', // userInfo에서 직접 가져오기
+      gender: getGenderText(userInfo.gender || ''), // userInfo에서 직접 가져오기
+      email: userInfo.email || '', // userInfo에서 직접 가져오기
+      phone: userInfo.phoneNumber || '', // userInfo에서 직접 가져오기
+    },
+    petOptions: petOptions,
+    petInfo: petInfo,
+    onPetSelect: onPetSelect,
+  };
 
   return (
-    <div
-      className="relative flex flex-col min-h-screen bg-center bg-cover"
-      style={{ backgroundImage: `url(${bgImage})` }}
-    >
-      <Header type={headerType} className="sticky top-0 z-50" />
-      <Glass
-        variant={isMobile ? 'mobile' : isTabletOrMobile ? 'tablet' : 'desktop'}
-        currentPage={1}
-        totalPages={1}
-        onPageChange={(newPage) => console.log('Page changed to:', newPage)}
-        showPageIndicator={false}
+    <div className='relative z-10 flex-grow my-4'>
+      <UserProfile
+        {...userProfileData}
+        onLeftIconClick={() => navigate(-1)} // 이전 페이지로 이동하는 핸들러 추가
       />
-      <div className="relative z-10 my-4">
-        <UserProfile {...userProfileData} />
-      </div>
-      <Footer type={footerType} className="relative z-10 mt-auto" />
     </div>
   );
 };
