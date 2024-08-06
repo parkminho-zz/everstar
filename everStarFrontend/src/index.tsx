6fee43bff26b7f12c04410b139bbbf5e2cac5d9b
정현조 <j96263732@gmail.com>
2024-08-06 09:27:29 +0900

    fix : index.tsx 복원

--- everStarFrontend/src/index.tsx
+++ everStarFrontend/src/index.tsx
@@ -1,5 +1,4 @@
 // index.tsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -12,9 +11,12 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <Provider store={Store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={Store}>
+      <App />
+    </Provider>
+    ,
+  </React.StrictMode>,
 );
 
 reportWebVitals();
