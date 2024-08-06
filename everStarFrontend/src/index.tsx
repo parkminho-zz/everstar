c40b9600ac39033d60ca74094ad5a264f3eb35cd
정현조 <j96263732@gmail.com>
2024-08-06 09:31:11 +0900

    fix : index.tsx 폰트 복원

--- everStarFrontend/src/index.tsx
+++ everStarFrontend/src/index.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '@fontsource/noto-sans-kr/400.css';
+import '@fontsource/noto-sans-kr/700.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -15,7 +17,6 @@ root.render(
     <Provider store={Store}>
       <App />
     </Provider>
-    ,
   </React.StrictMode>,
 );
 
