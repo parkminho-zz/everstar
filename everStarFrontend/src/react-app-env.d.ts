e92bdc4d113c1ef28de81c60c0f0ce4224d0bd16
박민호 <qkralsgh5508@naver.com>
2024-08-06 12:53:49 +0900

    feat:Fcm-Alarm-Develop

--- everStarFrontend/src/react-app-env.d.ts
+++ everStarFrontend/src/react-app-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="react-scripts" />
+
+interface ImportMetaEnv {
+  REACT_APP_FIREBASE_API_KEY?: string;
+  REACT_APP_FIREBASE_AUTHDOMAIN?: string;
+  REACT_APP_FIREBASE_PROJECTID?: string;
+  REACT_APP_FIREBASE_STORAGEBUCKET?: string;
+  REACT_APP_FIREBASE_MESSAGINGSENDERID?: string;
+  REACT_APP_FIREBASE_APPID?: string;
+  REACT_APP_FIREBASE_MEASUREMENTID?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
