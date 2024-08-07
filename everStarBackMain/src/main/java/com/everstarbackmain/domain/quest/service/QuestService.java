f6b93b96bdcda924d7e81351be946c38a19b8b55
kim doi <thisisdoemail@gmail.com>
2024-08-08 04:07:51 +0900

    Feature:퀘스트 서비스 annotation

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/quest/service/QuestService.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/quest/service/QuestService.java
@@ -1,4 +1,14 @@
 package com.everstarbackmain.domain.quest.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
 public class QuestService {
 }
