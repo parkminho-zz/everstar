2fa44eee87287cdc3ab292195de5153788d4388f
jjongbbang2 <jjongbbang2@naver.com>
2024-08-07 16:11:30 +0900

    "Feature : fcm 저장 컨트롤러"

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/notification/controller/NotificationController.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/notification/controller/NotificationController.java
@@ -20,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/notification")
+@RequestMapping("/api/notifications")
 @Slf4j(topic = "elk")
 public class NotificationController {
 
