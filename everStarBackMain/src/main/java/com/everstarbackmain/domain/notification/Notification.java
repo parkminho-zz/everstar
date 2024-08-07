e5053de113e9b20b45db522405def7f79e322c66
kim doi <thisisdoemail@gmail.com>
2024-08-07 16:15:37 +0900

    Feature: 알림 도메인 생성

---
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/notification/Notification.java
@@ -0,0 +1,11 @@
+package com.everstarbackmain.domain.notification;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Notification {
+
+
+}
---
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/notification/NotificationService.java
@@ -0,0 +1,4 @@
+package com.everstarbackmain.domain.notification;
+
+public class NotificationService {
+}
