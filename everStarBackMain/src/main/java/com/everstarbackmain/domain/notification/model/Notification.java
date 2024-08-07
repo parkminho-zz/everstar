117db467d73f604df24eee39c74cd1ed86245c66
jjongbbang2 <jjongbbang2@naver.com>
2024-08-07 09:47:30 +0900

    Feature : notification 리퍼지토리 작성

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/notification/model/Notification.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/notification/model/Notification.java
@@ -4,11 +4,12 @@ import com.everstarbackmain.domain.user.model.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -24,7 +25,7 @@ public class Notification {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@OneToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private User user;
 
