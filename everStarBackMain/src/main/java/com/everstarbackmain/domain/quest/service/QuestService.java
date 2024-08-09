b0be6e37f39ef8a240805c4e575ce85b1af4de8d
kim doi <thisisdoemail@gmail.com>
2024-08-09 11:04:53 +0900

    Feature: 스케줄러에서 호출 할 함수 변경

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/quest/service/QuestService.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/quest/service/QuestService.java
@@ -34,21 +34,13 @@ public class QuestService {
 	}
 
 	@Transactional
-	public void receiveQuest(User user, Long petId) {
+	public void changePetQuestCompleted(Long petId) {
 		Pet pet = petRepository.findByIdAndIsDeleted(petId, false)
 			.orElseThrow(() -> new ExceptionResponse(CustomException.NOT_FOUND_PET_EXCEPTION));
 
-		int questIndex = pet.getQuestIndex();
-		if (questIndex > 49) {
-			throw new ExceptionResponse(CustomException.ALL_QUESTS_COMPLETED_EXCEPTION);
-		}
+		pet.setFalseIsQuestCompleted();
 
-		Quest quest = questRepository.findById((long)questIndex)
-			.orElseThrow(() -> new ExceptionResponse(CustomException.NOT_FOUND_QUEST_EXCEPTION));
 
-		// 퀘스트를 수신하고 처리하는 로직을 추가. 예: 새로운 퀘스트를 발행, SSE를 통해 알림
-		//SSE 여기 들어가야함
-		// 이후의 스케줄링 작업 (퀘스트 잘 처리되나 확인 필요 함)
 	}
 
 }
