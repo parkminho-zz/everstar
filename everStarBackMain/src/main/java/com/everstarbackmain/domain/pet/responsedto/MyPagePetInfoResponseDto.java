9c197f3f428152539516e63b86280076e2266b44
kim doi <thisisdoemail@gmail.com>
2024-08-09 11:00:41 +0900

    merge 이상하게 된 부분 수정

--- everStarBackMain/src/main/java/com/everstarbackmain/domain/pet/responsedto/MyPagePetInfoResponseDto.java
+++ everStarBackMain/src/main/java/com/everstarbackmain/domain/pet/responsedto/MyPagePetInfoResponseDto.java
@@ -24,7 +24,6 @@ public class MyPagePetInfoResponseDto {
 	private Integer age;
 	private Integer questIndex;
 	private LocalDate memorialDate;
-	private Integer questIndex;
 	private String relationship;
 	private String species;
 	private List<String> petPersonalities;
@@ -40,7 +39,6 @@ public class MyPagePetInfoResponseDto {
 			.age(pet.getAge())
 			.questIndex(pet.getQuestIndex())
 			.memorialDate(pet.getMemorialDate())
-			.questIndex(pet.getQuestIndex())
 			.relationship(pet.getRelationship())
 			.species(pet.getSpecies())
 			.petPersonalities(petPersonalities)
