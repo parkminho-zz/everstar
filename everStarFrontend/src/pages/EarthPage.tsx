205bbce3841d21f17db50f14b016dcbf38892103
정현조 <j96263732@gmail.com>
2024-08-12 11:06:45 +0900

    chore : 링크 보호 해제

--- everStarFrontend/src/pages/EarthPage.tsx
+++ everStarFrontend/src/pages/EarthPage.tsx
@@ -4,10 +4,7 @@ import { EarthMain } from 'components/templates/EarthMain';
 import { LetterBoxTemplate } from 'components/templates/LetterBoxTemplate';
 import { Header } from 'components/molecules/Header/Header';
 import { Footer } from 'components/molecules/Footer/Footer';
-import {
-  LetterColor,
-  LetterState,
-} from 'components/molecules/cards/LetterCard/LetterCard';
+import { LetterColor, LetterState } from 'components/molecules/cards/LetterCard/LetterCard';
 import { LetterDetailTemplate } from 'components/templates/LetterDetailTemplate';
 import { LetterWriteTemplate } from 'components/templates/LetterWriteTemplate';
 import { QuestRouter } from 'components/templates/QuestRouter';
@@ -18,6 +15,7 @@ import bgImage from 'assets/images/bg-earth.webp';
 import { RootState } from 'store/Store';
 import { useSelector } from 'react-redux';
 import { useFetchOtherPetDetails } from 'hooks/useEverStar';
+import { PrivateRoute, PetDetailsRoute } from 'ProtectedRoutes';
 
 interface PetProfile {
   name: string;
@@ -73,54 +71,89 @@ export const EarthPage: React.FC = () => {
   };
   return (
     <div
-      className='relative flex flex-col w-full min-h-screen bg-center bg-cover'
+      className="relative flex flex-col w-full min-h-screen bg-center bg-cover"
       style={{ backgroundImage: `url(${bgImage})` }}
     >
-      <Header type='earth' className='top-0 z-50' />
-      <div className='flex flex-col flex-grow'>
-        <div className='flex items-center justify-center flex-grow h-full'>
+      <Header type="earth" className="top-0 z-50" />
+      <div className="flex flex-col flex-grow">
+        <div className="flex items-center justify-center flex-grow h-full">
           <Routes>
             <Route
-              path='/'
+              path="/"
               element={
-                <EarthMain
-                  title={petDetails.name}
-                  fill={petProfile.questIndex}
-                  buttonSize='large'
-                  buttonDisabled={false}
-                  buttonText='영원별로 이동'
-                  buttonIcon='SmallStarImg'
-                  onButtonClick={() => console.log('영원별 이동')}
-                />
+                <PrivateRoute>
+                  <PetDetailsRoute>
+                    <EarthMain
+                      title={petDetails.name}
+                      fill={petProfile.questIndex}
+                      buttonSize="large"
+                      buttonDisabled={false}
+                      buttonText="영원별로 이동"
+                      buttonIcon="SmallStarImg"
+                      onButtonClick={() => console.log('영원별 이동')}
+                    />
+                  </PetDetailsRoute>
+                </PrivateRoute>
               }
             />
+
             <Route
-              path='letterbox'
+              path="letterbox"
               element={
-                <LetterBoxTemplate
-                  letterData={generateLargeLetterData(50)}
-                  currentPage={1}
-                  totalPages={9}
-                  onPageChange={() => console.log('이동1')}
-                  headerText='편지함'
-                />
+                <PrivateRoute>
+                  <PetDetailsRoute>
+                    <LetterBoxTemplate
+                      letterData={generateLargeLetterData(50)}
+                      currentPage={1}
+                      totalPages={9}
+                      onPageChange={() => console.log('이동1')}
+                      headerText="편지함"
+                    />
+                  </PetDetailsRoute>
+                </PrivateRoute>
               }
             />
-            <Route path='letter' element={<LetterWriteTemplate />} />
-            <Route path='letter/:id' element={<LetterDetailTemplate />} />
-            <Route path='quest/:questid' element={<QuestRouter />} />
+
             <Route
-              path='openvidu/:questid'
-              element={<QuestOpenviduTemplate />}
+              path="letter"
+              element={
+                <PrivateRoute>
+                  <PetDetailsRoute>
+                    <LetterWriteTemplate />
+                  </PetDetailsRoute>
+                </PrivateRoute>
+              }
             />
-            <Route path='openvidu/sessionid' element={<OpenViduApp />} />
+
             <Route
-              path='openvidu/sessionid/:sessionId'
-              element={<OpenViduApp />}
+              path="letter/:id"
+              element={
+                <PrivateRoute>
+                  <PetDetailsRoute>
+                    <LetterDetailTemplate />
+                  </PetDetailsRoute>
+                </PrivateRoute>
+              }
             />
+
+            <Route
+              path="quest/:questid"
+              element={
+                <PrivateRoute>
+                  <PetDetailsRoute>
+                    <QuestRouter />
+                  </PetDetailsRoute>
+                </PrivateRoute>
+              }
+            />
+
+            {/* 보호되지 않은 경로들 */}
+            <Route path="openvidu/:questid" element={<QuestOpenviduTemplate />} />
+            <Route path="openvidu/sessionid" element={<OpenViduApp />} />
+            <Route path="openvidu/sessionid/:sessionId" element={<OpenViduApp />} />
           </Routes>
         </div>
-        <Footer className='w-full mt-auto' />
+        <Footer className="w-full mt-auto" />
       </div>
     </div>
   );
